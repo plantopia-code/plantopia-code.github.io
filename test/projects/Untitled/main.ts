7274fa6f79ab9bd2f4467cc03ef302ed46ebf80c
gitshanks <nishankatwork@gmail.com>
2020-06-27 18:24:53 -0600

    removed test

--- test/projects/Untitled/main.ts
+++
@@ -1,6 +0,0 @@
-loops.forever(function () {
-    turtle.turn(Direction.Left, 90)
-})
-turtle.onBump(function () {
-	
-})
--- test/projects/square/main.ts
+++
@@ -1,6 +0,0 @@
-loops.forever(function () {
-    for (let i = 0; i < 4; i++) {
-        turtle.forward(1)
-        turtle.turn(Direction.Left, 90)
-    }
-})
